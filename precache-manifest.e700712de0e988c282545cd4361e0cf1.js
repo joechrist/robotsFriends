bfabf2409c0cd203e9cb655dde1230ba95375d75
Joe <christowel@live.fr>
2020-01-14 21:24:48 -0500

    Updates

---
+++ precache-manifest.e700712de0e988c282545cd4361e0cf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d7fc90959b0403627c0cbde9f3d18c",
+    "url": "/robotsFriends/index.html"
+  },
+  {
+    "revision": "06d941795b1a946c9c5a",
+    "url": "/robotsFriends/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "6de64e14ea176e5d2836",
+    "url": "/robotsFriends/static/css/main.6175bc1f.chunk.css"
+  },
+  {
+    "revision": "06d941795b1a946c9c5a",
+    "url": "/robotsFriends/static/js/2.f445ed6e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robotsFriends/static/js/2.f445ed6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6de64e14ea176e5d2836",
+    "url": "/robotsFriends/static/js/main.c01efe65.chunk.js"
+  },
+  {
+    "revision": "f29fba409dd2081fa7b8",
+    "url": "/robotsFriends/static/js/runtime-main.1d25b354.js"
+  }
+]);
